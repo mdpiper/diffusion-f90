8d6ce216d0ddf3c556989a940ed36bb0f15d1b24
Mark Piper <mark.piper@colorado.edu>
2017-03-01 16:52:09 -0700

    Add build system with CMake

---
+++ diffusion/diffusion.pc.cmake
@@ -0,0 +1,5 @@
+Name: diffusion
+Description: An example of the diffusion equation in Fortran 90
+Version: ${DIFFUSION_VERSION}
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -ldiffusion
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include
