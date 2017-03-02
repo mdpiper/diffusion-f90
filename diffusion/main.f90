f6daf6dce4504c920f502486502b5d449308c77c
Mark Piper <mark.piper@colorado.edu>
2017-03-02 16:37:44 -0700

    Show diffusion in main program

--- diffusion/main.f90
+++ diffusion/main.f90
@@ -4,10 +4,21 @@ program main
   implicit none
 
   type (diffusion_model) :: model
+  integer :: i
 
-  write (*,*) "Start."
+  write(*,"(a)") "Start"
   call initialize_from_defaults(model)
+
+  write(*,"(a)") "Model initial values..."
+  call print_values(model)
+
+  do while (model%t < model%t_end)
+     call advance_in_time(model)
+     write(*,"(a, f6.1)") "Model values at time = ", model%t
+     call print_values(model)
+  end do
+
   call cleanup(model)
-  write (*,*) "Finish."
+  write(*,"(a)") "Finish"
 
 end program main
