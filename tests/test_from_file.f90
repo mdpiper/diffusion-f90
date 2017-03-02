d1dbed74d4929e2937e331fcd0af298b0c0e5911
Mark Piper <mark.piper@colorado.edu>
2017-03-02 10:17:38 -0700

    Print model information in tests

--- tests/test_from_file.f90
+++ tests/test_from_file.f90
@@ -5,14 +5,31 @@ program test_model_sanity_from_file
 
   type (diffusion_model) :: m
   character (len=8) :: cfg_file = "test.cfg"
+  integer :: i, j
+  character(len=30) :: rowfmt
 
-  write (*,"(a20, a8)") "Configuration file: ", cfg_file
-  write (*,"(a)", advance="no") "Initializing..."
+  write(*,"(a20, a8)") "Configuration file: ", cfg_file
+  write(*,"(a)", advance="no") "Initializing..."
   call initialize_from_file(m, cfg_file)
-  write (*,*) "Done."
+  write(*,*) "Done."
 
-  write (*,"(a)", advance="no") "Cleaning up arrays..."
+  write(*,"(a)") "Model info..."
+  write(*,"(a10, i8)") "n_x:", m%n_x
+  write(*,"(a10, i8)") "n_y:", m%n_y
+  write(*,"(a10, f8.2)") "dx:", m%dx
+  write(*,"(a10, f8.2)") "dy:", m%dy
+  write(*,"(a10, f8.2)") "dt:", m%dt
+  write(*,"(a10, f8.2)") "t:", m%t
+  write(*,"(a10, f8.2)") "t_end:", m%t_end
+
+  write(*,"(a)") "Model initial values..."
+  write(rowfmt,'(a,i4,a)') '(', m%n_x, '(1x,f4.1))'
+  do i = 1, m%n_y
+     write(*,fmt=rowfmt) (m%density(i,j), j=1,m%n_x)
+  end do
+
+  write(*,"(a)", advance="no") "Cleaning up arrays..."
   call cleanup(m)
-  write (*,*) "Done"
+  write(*,*) "Done"
 
 end program test_model_sanity_from_file
