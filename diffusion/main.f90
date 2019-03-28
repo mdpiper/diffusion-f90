b4425f0dc600e9eb6b8e3f5f92fe792bdc3c8132
Mark Piper <mark.piper@colorado.edu>
2019-03-28 10:02:50 -0600

    Add command line argument
    A config file is now needed to run the main program. The command line
    arg mechanism is really simple, and it doesn't do any checking on the
    config file.

--- diffusion/main.f90
+++ diffusion/main.f90
@@ -3,10 +3,24 @@ program main
   use diffusion
   implicit none
 
+  integer :: arg_count = 0
+  character (len=80) :: arg
   type (diffusion_model) :: model
 
+  do while (arg_count <= 1)
+    call get_command_argument(arg_count, arg)
+    arg_count = arg_count + 1
+  end do
+
+  if (len_trim(arg) == 0) then
+     write(*,"(a)") "Usage: run_diffusionf CONFIGURATION_FILE"
+     write(*,"(a)")
+     write(*,"(a)") "Run the diffusionf model with a configuration file."
+     return
+  end if
+
   write(*,"(a)") "Start"
-  call initialize_from_defaults(model)
+  call initialize_from_file(model, arg)
 
   write(*,"(a)") "Model initial values..."
   call print_values(model)
